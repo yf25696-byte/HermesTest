33c680c252dee0bd219ad58431344a556adad7ce
yf25696-byte <yf25696@gmail.com>
2026-06-15 02:38:16 +0800

    Finalizing ocean theme and automation

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div className="text-3xl font-bold underline text-blue-600">
+      Welcome to Free Island 自由島嶼
+    </div>
+  </React.StrictMode>
+);
